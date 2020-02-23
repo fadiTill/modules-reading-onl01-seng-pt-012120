99a3632471f7a258bc85f0847a69ff60e78ffad6
fadimata tillman <fadi.tillman@gmail.com>
2020-02-23 18:08:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,16 +1,26 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
+# class Kid
+#   extend FancyDance::ClassMethods
+#   include FancyDance::InstanceMethods
+#   extend MetaDancing
+#   include Dance
+#
+#   attr_accessor :name
+#
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+# end
 class Kid
-  extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
-  extend MetaDancing
   include Dance
-
+  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   attr_accessor :name
-
   def initialize(name)
     @name = name
   end
-end
 end
