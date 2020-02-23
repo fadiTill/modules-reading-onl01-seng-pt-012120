cece934a873bb5ecfc0ee6a3c3a7fe746eec7b1c
fadimata tillman <fadi.tillman@gmail.com>
2020-02-23 18:16:09 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,9 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
   include Dance
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
